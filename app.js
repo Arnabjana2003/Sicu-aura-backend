edfabd8e551a1bd1a2c7c5841038d17784f81ba2
Arnab Jana <arnabjana0099@gmail.com>
2024-04-24 23:06:11 +0530

    cors update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "https://picpulse.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "https://sicu-aura-backend.vercel.app"];
 app.use(
   cors({
     origin: function (origin, callback) {
